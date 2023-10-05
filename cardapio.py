4f3eed07e0c332a8b94727a60da10c9ac86d2422
Victor Fernandes <70545624+devhmalu@users.noreply.github.com>
2023-10-05 09:29:02 -0300

    Add files via upload

---
+++ cardapio.py
@@ -0,0 +1,281 @@
+# snake_case para variáveis, funções e métodos;
+# PascalCase para classes;
+# SCREAMING_SNAKE_CASE para constantes.
+
+
+import random
+
+print("Bem vindo ao seu cardápio digital")
+
+print("\nQuantos dias você deseja gerar o menu?\n")
+
+escolha = input("\nDigite sua opção (1/2/3/4/5/6/7): ")
+
+
+proteinas = [
+    "Carne Moida, ",
+    "Frango Desfiado, ",
+    "Strogonoff de Frango, ",
+    "Strogonoff de Carne, ",
+    "Filé de Frango, ",
+    "Bolinha de Carne, ",
+    "Carne de Forno, ",
+    "Contra Filé, ",
+    "Alcatra, ",
+    "Frango com Molho de Tomate, ",
+]
+acompanhamento1 = ["Arroz, "]
+acompanhamento2 = ["Feijão, "]
+acompanhamento3 = ["Pure de Batata"]
+verduras = [", Chuchu", ", Brócolis", ", Beterraba"]
+
+
+if escolha == "1":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+
+elif escolha == "2":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+    print(
+        "Dia 2: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+
+elif escolha == "3":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+    print(
+        "Dia 2: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 3: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+elif escolha == "4":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+    print(
+        "dia 2: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 3: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 4: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+elif escolha == "5":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+    print(
+        "Dia 2: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 3: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 4: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 5: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+elif escolha == "6":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+    print(
+        "Dia 2: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 3: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 4: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 5: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 6: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+elif escolha == "7":
+    print("\n")
+    print(
+        "Dia 1: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+        random.choice(verduras),
+    )
+    print("\n")
+    print(
+        "Dia 2: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 3: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 4: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 5: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 6: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+    print(
+        "Dia 7: ",
+        random.choice(proteinas),
+        random.choice(acompanhamento1),
+        random.choice(acompanhamento2),
+        random.choice(acompanhamento3),
+    )
+    print("\n")
+else:
+    print("Você não escolheu uma opção válida")
